2249d1c7620000029272cdc17327bdaaece50083
thyar <thyartismurder1711@gmail.com>
2024-12-07 17:06:50 +0300

    Домашняя работа по уроку "Пространство имен."

---
+++ TestF.py
@@ -0,0 +1,8 @@
+
+def  test_function ():
+    def inner_function ():
+        print("Я в области видимости функции test_function")
+
+    inner_function()# Вызов внутренней функции
+test_function()# Вызов test_function для демонстрации
+# inner_function() #выдас ошибку "NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?"
